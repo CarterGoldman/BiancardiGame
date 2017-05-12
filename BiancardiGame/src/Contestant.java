6b0d7ebcba2fbb810f1de1a46b7a3d1b129e6f34
maplaut <maddencoins999@gmail.com>
2017-05-12 13:33:39 -0500

    update
    update

--- BiancardiGame/src/Contestant.java
+++ BiancardiGame/src/Contestant.java
@@ -3,8 +3,23 @@
  * 
  */
 
-public class Contestant
+public class Contestant 
 {
 	private int points;
+	private String name;
+	private int height;
+	private int weight;
+	private int IQ;
+	private double GPA;
 	
+	
+	public Contestant(int p, String nam, int h,int w, int iq, double gpa)
+	{
+		points = p;
+		name = nam;
+		height = h;
+		weight = w;
+		IQ = iq;
+		GPA = gpa;
+	}
 }
