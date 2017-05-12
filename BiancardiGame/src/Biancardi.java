1db6ee209f553fa10bf023322e517b539e977695
CarterGoldman <0cardog0@gmail.com>
2017-05-12 13:55:37 -0500

    Biancardi

--- BiancardiGame/src/Biancardi.java
+++ BiancardiGame/src/Biancardi.java
@@ -7,10 +7,17 @@ public class Biancardi
 {
 	private int IQ;
 	private double GPA;
+	private Question questions;
 	
 	public Biancardi()
 	{
 		IQ = 500;
 		GPA = 5.0;
+		questions = new Question();
+	}
+	
+	public String askNextQuestion()
+	{
+		return questions.generateQuestion();
 	}
 }
