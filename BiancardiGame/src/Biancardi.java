80f1869263d9b000fa930a29d61100e6bb518a69
CarterGoldman <0cardog0@gmail.com>
2017-05-17 13:43:50 -0500

    Pictures nd stuff

--- BiancardiGame/src/Biancardi.java
+++ BiancardiGame/src/Biancardi.java
@@ -7,17 +7,15 @@ public class Biancardi
 {
 	private int IQ;
 	private double GPA;
-	private Question questions;
 	
 	public Biancardi()
 	{
 		IQ = 500;
 		GPA = 5.0;
-		questions = new Question();
 	}
 	
-	public String askNextQuestion()
+	public String comment()
 	{
-		return questions.generateQuestion();
+		return "test";
 	}
 }
