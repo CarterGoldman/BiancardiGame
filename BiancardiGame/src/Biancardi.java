f3621175a72fba29e096735d0c8ad528d34b864b
Silamoth <michaelbiancardi@icloud.com>
2017-05-17 16:19:37 -0500

    Added a semicolon to Biancardi that Carter forgot

--- BiancardiGame/src/Biancardi.java
+++ BiancardiGame/src/Biancardi.java
@@ -7,7 +7,7 @@ public class Biancardi
 {
 	private int IQ;
 	private double GPA;
-	private String[] comments = {"Heck", "My name is Biancardi and I'm ready to frickin party", "My GPA is " + GPA, " so I'm smarter than you", "I have a " + IQ + " IQ so I am better than you", "My" + GPA + " GPA is okay"}
+	private String[] comments = {"Heck", "My name is Biancardi and I'm ready to frickin party", "My GPA is " + GPA, " so I'm smarter than you", "I have a " + IQ + " IQ so I am better than you", "My" + GPA + " GPA is okay"};
 	
 	public Biancardi()
 	{
