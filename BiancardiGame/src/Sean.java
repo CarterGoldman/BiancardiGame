9a0719a7dfcbcbedca04852bbbbc4c648f844455
Bianparty <michaelbiancardi@icloud.com>
2017-05-23 13:15:58 -0500

    Added Sean class. Dylan needs to chill

--- BiancardiGame/src/Sean.java
+++ BiancardiGame/src/Sean.java
@@ -4,10 +4,210 @@
  */
 public class Sean extends Contestant
 {
-
-	public Sean() 
+	private int points;
+	private String name;
+	private String answer;
+	private String question;
+	
+	public Sean(String name)
 	{
-		super("Sean");
+		super(name);
 	}
 	
+	public void addPoints(int amount)
+	{
+		points += amount;
+	}
+	public int getPoints()
+	{
+		return points;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String getAnswer()
+	{
+		return answer;
+	}
+	public String getQuestion()
+	{
+		return question;
+	}
+	public void setQuestion()
+	{
+		question = Question.generateQuestion();
+	}
+	public String answer(int questionNum)
+	{
+		switch(questionNum)
+		{
+		case 1:
+			answer = " ";
+			answer = "A day at the VFW with muscular women";
+		break;
+		case 2:
+			answer = " ";
+			answer = "As a high school principal my GPA supersedes all of yours";
+		break;
+		case 3:
+			answer = " ";
+			answer = "I am too busy being a principal to deal with those small activates";
+		break;
+		case 4:
+			answer = " ";
+			answer = "What do you think I do in my office all day ";
+		break;
+		case 5:
+			answer = " ";
+			answer = "No, I am bald";
+		break;
+		case 6:
+			answer = " ";
+			answer = "I use my booming voice to assert my dominance over the youth";
+		break;
+		case 7:
+			answer = " ";
+			answer = "Driving my Hummer to the gym and getting built";
+		break;
+		case 8:
+			answer = " ";
+			answer = "I have one on my phone that’s strapped to my waist at all times";
+		break;
+		case 9:
+			answer = " ";
+			answer = "As I stated earlier, what do you think I do all day";
+		break;
+		case 10:
+			answer = " ";
+			answer = "Big";
+		break;
+		case 11:
+			answer = " ";
+			answer = "The United States of America, the country that I know and love.  GOD BLESS AMERICA!";
+		break;
+		case 12:
+			answer = " ";
+			answer = "Fine, I guess you’re gonna miss…the panty raid";
+		break;
+		case 13:
+			answer = " ";
+			answer = "Bald Eagle";
+		break;
+		case 14:
+			answer = " ";
+			answer = "My House";
+		break;
+		case 15:
+			answer = " ";
+			answer = "Football Season";
+		break;
+		case 16:
+			answer = " ";
+			answer = "American";
+		break;
+		case 17:
+			answer = " ";
+			answer = "I prefer starbucks";
+		break;
+		case 18:
+			answer = " ";
+			answer = "Born in the USA";
+		break;
+		case 19:
+			answer = " ";
+			answer = "Everyday in my Hummer";
+		break;
+		case 20:
+			answer = " ";
+			answer = "Shooting the ring out of a gun";
+		break;
+		case 21:
+			answer = " ";
+			answer = "18 when I joined the army";
+		break;
+		case 22:
+			answer = " ";
+			answer = "Patriotism";
+		break;
+		case 23:
+			answer = " ";
+			answer = "Red, white, and blue";
+		break;
+		case 24:
+			answer = " ";
+			answer = "American Dad";
+		break;
+		case 25:
+			answer = " ";
+			answer = "Every student here at Lake Central is my child";
+		break;
+		case 26:
+			answer = " ";
+			answer = "For us to conquer and destroy the Middle East";
+		break;
+		case 27:
+			answer = " ";
+			answer = "Rid the world of Infidels";
+		break;
+		case 28:
+			answer = " ";
+			answer = "Sean Bagels";
+		break;
+		case 29:
+			answer = " ";
+			answer = "George Washington, Lady Liberty, Uncle Sam";
+		break;
+		case 30:
+			answer = " ";
+			answer = "The Bible";
+		break;
+		case 31:
+			answer = " ";
+			answer = "I go to the range";
+		break;
+		case 32:
+			answer = " ";
+			answer = "My large stature";
+		break;
+		case 33:
+			answer = " ";
+			answer = "No";
+		break;
+		case 34:
+			answer = " ";
+			answer = "-10";
+		break;
+		case 35:
+			answer = " ";
+			answer = "Too often";
+		break;
+		case 36:
+			answer = " ";
+			answer = "The Statue of Liberty";
+		break;
+		case 37:
+			answer = " ";
+			answer = "George Washington";
+		break;
+		case 38:
+			answer = " ";
+			answer = "Freedom";
+		break;
+		case 39:
+			answer = " ";
+			answer = "Minions";
+		break;
+		case 40:
+			answer = " ";
+			answer = "What’s a pokerman";
+		break;
+		case 41:
+			answer = " ";
+			answer = "Hamburgers and Hotdogs";
+		break;
+
+		}
+		return answer = answer.toLowerCase();
+	}
 }
